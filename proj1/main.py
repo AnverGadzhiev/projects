b38e3066249ab0e0223fe40ea2f0b59c68e2bfcf
Anver Gadjiev <whitenuclearrhino@gmail.com>
2022-03-01 09:41:58 +0300

    New Class

--- proj1/main.py
+++ proj1/main.py
@@ -1,12 +1,16 @@
-from car import *
+from stack import *
 
 def main():
-    lada = Bucket(False, True, True, False)
-    # lada.pr()
-    print(lada.get_state())
-    lada.repair(1, 4)
-    print(lada.get_state(), lada.get_garage_state())
-    del lada
+    stc = Stack()
+    bucket = Stack(1, 'fds', 4.123)
+    bucket.append(21, '313')
+    bucket.append()
+    print(bucket.length())
+    print(bucket.pop())
+    print(bucket.length())
+    print(bucket.get_top())
+    print(bucket.set_top(12121))
+    
 
 if __name__ == '__main__':
     main()
